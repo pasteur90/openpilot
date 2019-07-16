1bc3608f20d4275bca7843cb2d477eba4d9a4b5c
Thomas Fifer <thomasfiferjr@gmail.com>
2019-07-15 20:08:11 -0500

    hardcode min_steer_speed to 32mph

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -236,12 +236,7 @@ class CarState(object):
     self.car_gas = cp.vl["EMS12"]['TPS']
 
     # Learn Minimum Steer Speed
-    if self.mdps12_flt != 0 and self.v_ego_raw > 0. and abs(self.angle_steers) < 5.0 and self.lkas11_icon != 2:
-      if self.v_ego_raw > self.min_steer_speed:
-        self.min_steer_speed = self.v_ego_raw + 0.1
-    # If we have LKAS_Icon == 2, then we know its 16.7m/s
-    elif self.lkas11_icon == 2 and self.min_steer_speed < 16.7:
-      self.min_steer_speed = 16.7
+    self.min_steer_speed = 14.3
 
     # Gear Selecton - This is not compatible with all Kia/Hyundai's, But is the best way for those it is compatible with
     gear = cp.vl["LVR12"]["CF_Lvr_Gear"]
