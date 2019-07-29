cb1605e099fa8e6948a2a90bda1182c3a1f89aa7
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-07-30 02:42:46 +0300

    hyundai Radar

--- selfdrive/car/hyundai/radar_interface.py
+++ selfdrive/car/hyundai/radar_interface.py
@@ -30,6 +30,7 @@ class RadarInterface(object):
     self.rcp = get_radar_can_parser(CP)
     self.trigger_msg = self.rcp.vl["SCC11"]['ACC_ObjStatus']
     self.updated_messages = set()
+    self.no_radar: False
 
   def update(self, can_strings):
     if self.no_radar:
@@ -48,3 +49,24 @@ class RadarInterface(object):
     self.updated_messages.clear()
 
     return rr
+  
+  
+  def _update(self, updated_messages):
+    ret = car.RadarData.new_message()
+    cpt = self.rcp.vl
+    errors = []
+    if not self.rcp.can_valid:
+      errors.append("canError")
+    ret.errors = errors
+
+    valid = cpt["SCC11"]['ACC_ObjStatus']
+    if valid:
+      self.pts.dRel = cpt['LONG_DIST']  # from front of car
+      self.pts.yRel = -cpt['LAT_DIST']  # in car frame's y axis, left is positive
+      self.pts.vRel = cpt['REL_SPEED']
+      self.pts.aRel = float('nan')
+      self.pts.yvRel = float('nan')
+      self.pts.measured = True
+
+    ret.points = self.pts.values()
+    return ret
